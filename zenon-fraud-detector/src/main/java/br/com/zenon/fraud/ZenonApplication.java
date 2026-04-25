767f3a37c865bc1aab00db15656c65175b7c5f9e
PaulaBuscacio <paula@buscacio.net>
2026-04-25 17:53:55 -0300

    usando o construtor no lugar do metodo

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ZenonApplication.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ZenonApplication.java
@@ -15,8 +15,7 @@ public class ZenonApplication {
         .limit(10)
         .forEach(System.out::println);
 
-//    Transaction t = new Transaction(1, "PAYMENT", null, null, null, "0", "0");
-//    System.out.println(t);
+
   }
 
 
