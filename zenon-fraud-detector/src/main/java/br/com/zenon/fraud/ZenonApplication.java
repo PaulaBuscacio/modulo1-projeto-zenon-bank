4a4bfe318d26a38b04e335a7adf89750f36c8040
PaulaBuscacio <paula@buscacio.net>
2026-04-29 22:38:14 -0300

    relatorio com java nio

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ZenonApplication.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ZenonApplication.java
@@ -1,6 +1,10 @@
 package br.com.zenon.fraud;
 
+import br.com.zenon.fraud.domain.Transaction;
+import br.com.zenon.fraud.infra.TransactionIngestor;
 import br.com.zenon.fraud.infra.TransactionListRepositoryImpl;
+
+import java.util.List;
 
 import static java.lang.IO.println;
 
@@ -8,17 +12,9 @@ public class ZenonApplication {
   static void main() {
 
 
-    TransactionListRepositoryImpl transactionRepository = new TransactionListRepositoryImpl();
-
-    transactionRepository.printTransaction("C12345");
-    transactionRepository.printTransaction("C1231006815");
-    Long searchTime = transactionRepository.getSearchTimeNameOrigiLastTransaction("data/PS_20174392719_1491204439457_log.csv", 1000000);
-
-    println("---------------------------------------------------------------------------------------------");
-    println("Tempo de busca da última transação usando lista: " + searchTime + " nanosegundos");
-    Long searchTimeMap = transactionRepository.getSearchTimeNameOrigiLastTransaction("data/PS_20174392719_1491204439457_log.csv", 1000000);
-    println("Tempo de busca da última transação usando mapa: " + searchTimeMap + " nanosegundos");
-    println("---------------------------------------------------------------------------------------------");
-    println("Map é mais rápido que list? " + (searchTimeMap < searchTime));
+    TransactionIngestor transactionIngestor = new TransactionIngestor();
+    //java.lang.OutOfMemoryError: Java heap space usando -Xmx128m como argumento
+    List<Transaction> transactions = transactionIngestor.extractTransactionFromFile("data/PS_20174392719_1491204439457_log.csv", null);
+    println(transactions);
   }
 }
--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/infra/TransactionIngestor.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/infra/TransactionIngestor.java
@@ -4,12 +4,15 @@ import br.com.zenon.fraud.domain.vo.TransactionCustomer;
 import br.com.zenon.fraud.domain.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.IO.println;
 
 /**
  * responsável pela ingestão de dados de transaçōes do PaySim.
@@ -47,8 +50,44 @@ public class TransactionIngestor {
             System.err.println("Error: " + line + " | " + e.getClass() + ": " + e.getMessage());
           }
         });
-
     return transactions;
+  }
+
+  public List<Transaction> extractTransactionFromFileLazy(String fileName, Integer limit) {
+    List<String> lines;
+    List<Transaction> transactions = new ArrayList<>();
+    try {
+      lines = Files.readAllLines(Path.of(fileName));
+    } catch (IOException e) {
+      throw new RuntimeException("Nao foi possível ler o arquivo" + fileName, e);
+    }
+    if (lines.isEmpty()) {
+      log.warn("O arquivo {} está vazio", fileName);
+      return transactions;
+    }
+    int maxValue = limit == null ? lines.size() - 1 : limit;
+    lines.stream()
+        .skip(1)
+        .limit(maxValue)
+        .forEach(line -> {
+          String[] fields = line.split(",");
+          try {
+            TransactionCustomer originTransactionCustomer = new TransactionCustomer(fields[3], fields[4], fields[5]);
+            TransactionCustomer destTransactionCustomer = new TransactionCustomer(fields[6], fields[7], fields[8]);
+            transactions.add(new Transaction(Integer.valueOf(fields[0]), fields[1], fields[2], originTransactionCustomer, destTransactionCustomer, fields[9], fields[10]));
+          } catch (RuntimeException e) {
+            System.err.println("Error: " + line + " | " + e.getClass() + ": " + e.getMessage());
+          }
+        });
+    return transactions;
+  }
+
+  static void main() {
+    TransactionIngestor transactionIngestor = new TransactionIngestor();
+    transactionIngestor.extractTransactionFromFileLazy("data/PS_20174392719_1491204439457_log.csv", 1000000);
+    transactionIngestor.extractTransactionFromFile("data/PS_20174392719_1491204439457_log.csv", 1000000);
+
+
   }
 
 }
