b0b4f2623887700a55376724f99141138838c693
PaulaBuscacio <paula@buscacio.net>
2026-05-01 22:54:27 -0300

    inclusao de resource bundle

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
@@ -2,20 +2,19 @@ package br.com.zenon.fraud;
 
 import br.com.zenon.fraud.useCase.TransactionReport;
 
-import static java.lang.IO.println;
+import java.util.Locale;
 
 public class ReportMain {
 
   static void main() {
+
     TransactionReport transactionReport = new TransactionReport();
-    String report = transactionReport.printReport("data/PS_20174392719_1491204439457_log.csv", null);
+    String report = transactionReport.printReport("data/PS_20174392719_1491204439457_log.csv", null, null);
     System.out.println(report);
+    String reportUS = transactionReport.printReport("data/PS_20174392719_1491204439457_log.csv", null, Locale.US);
+    System.out.println(reportUS);
+    String reportBR = transactionReport.printReport("data/PS_20174392719_1491204439457_log.csv", null, Locale.of("pt", "BR"));
+    System.out.println(reportBR);
 
-    //Benchmark extração de arquivo usando Files.readString vs Files.readAllLines
-   String benchmark = transactionReport.benchmarkReport("data/PS_20174392719_1491204439457_log.csv", null);
-
-   println("\n-----------------Files.readString vs Files.readAllLines--------------------------------------");
-
-   System.out.println(benchmark);
   }
 }
--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/useCase/TransactionReport.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/useCase/TransactionReport.java
@@ -4,39 +4,30 @@ import br.com.zenon.fraud.domain.Transaction;
 import br.com.zenon.fraud.infra.TransactionIngestor;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
 public class TransactionReport {
 
   TransactionIngestor transactionIngestor = new TransactionIngestor();
 
 
-  public String printReport(String fileName, Integer limit) {
+  public String printReport(String fileName, Integer limit, Locale locale) {
+    locale = Optional.ofNullable(locale).orElse(Locale.of("pt", "BR"));
+    ResourceBundle rb = ResourceBundle.getBundle("report", locale);
+    NumberFormat format = NumberFormat.getCurrencyInstance(locale);
     List<Transaction> transactions = transactionIngestor.extractTransactionFromFileLazy(fileName, limit);
     int totalFrauds = transactions.stream().filter(Transaction::isFraud).toList().size();
     BigDecimal totalTransactionsValues =
         transactions.stream().map(Transaction::amount).reduce(BigDecimal.ZERO, BigDecimal::add);
 
-    return "Total de linhas: " + transactions.size() +
-        "\nTotal de fraudes: " + totalFrauds +
-        "\nValor total transacionado: " + totalTransactionsValues;
+    return rb.getString("total.lines") + ": " + transactions.size() +
+        "\n" + rb.getString("total.frauds") + ": " + totalFrauds +
+        "\n" + rb.getString("total.value") + ": " + format.format(totalTransactionsValues);
 
   }
-
-  public String benchmarkReport(String fileName, Integer limit) {
-    long startTime1 = System.currentTimeMillis();
-    transactionIngestor.extractTransactionFromFile(fileName, limit);
-    long endTime1 = System.currentTimeMillis();
-    long startTime2 = System.currentTimeMillis();
-    transactionIngestor.extractTransactionFromFileLazy(fileName, limit);
-    long endTime2 = System.currentTimeMillis();
-
-    long duration1 = endTime1 - startTime1;
-    long duration2 = endTime2 - startTime2;
-
-    return "Tempo de execução usando Files.readString: " + duration1 + " ms"
-        + "\nTempo de execução usando Files.readAllLines: " + duration2 + " ms";
-  }
-
 
 }
